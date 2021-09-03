1ed3ca53e9ee3ebcd228b5854b5e2c467739eb71
insungcho1939 <insungcho1939@gmail.com>
2021-09-03 13:41:30 +0900

    호스트 추가, 서버 로직 변경

--- tcpClient.js
+++ tcpClient.js
@@ -1,21 +1,20 @@
-var net = require('net');
+let net = require('net');
 
-var options = { // 접속 정보 설정
+let options = { // 접속 정보 설정
   port: 80,
-//   host: "www.google.com",
-  host: 'localhost'
+  host: "localhost"
+//     host: 'localhost'
 };
 
-var client = net.connect(options, () => { // 서버 접속
+let client = net.connect(options, () => { // 서버 접속
   console.log("서버에 접속 성공했습니다.");
-  client.write('GET / HTTP/1.1\nHost: www.google.com\n\n')
+  client.write('GET / HTTP/1.1\nHost: finset2.io\n\n')
 });
 
 //client.send
 
 client.on('data', (data) => { // 데이터 수신 이벤트
   console.log(data.toString());
-  client.end()
 });
 
 client.on('end', () => { // 접속 종료
--- tcpServer.js
+++ tcpServer.js
@@ -1,15 +1,44 @@
-var net = require('net'); // net 모듈 로드
+let net = require('net'); // net 모듈 로드
+let fs = require('fs'); // fs 모듈 로드
 
-var server = net.createServer((socket) => { // TCP 서버를 만든다.
-//   socket.end('hello insung!');
+let server = net.createServer((socket) => { // TCP 서버를 만든다.
     socket.on('data', function(data) {
         const lines = data.toString().split('\n')
-        const path = lines[0].split(' ')[1]
+        const domain = lines[1].split(' ')[1].trim()
+        let path = lines[0].split(' ')[1]
+        console.log('data: ' + data)
         console.log('line: ' + lines[0])
+        console.log('domain: ' + domain)
         console.log('path: ' + path)
-	    // console.log('Received: ' + data);
-	    socket.write('HTTP/1.1 200 OK\n\n<html><body><h1>hello insung</h1></body></html>');
-        socket.end()
+        
+        if(1 || domain == "insung.com"){
+            if (path == '/') {
+                path = '/index.html'
+            }
+            const winPath = `c:\\insung.com${path}`;
+                
+            let option = null; // {encoding:'base64', flag:'r'};
+            fs.readFile(winPath, option, (err, body) => {
+                if(err){
+                    socket.write(`HTTP/1.1 404 NOT FOUND\n\n`);
+                }
+            console.log('body:', winPath, body);
+            if (path == '/finset.png' || path == '/google.png') {
+                socket.write(`HTTP/1.1 200 OK\ncontent-type: image/png\ncontent-length: ${body.length}\n\n`);
+                socket.write(body, 'base64');
+            } else {
+              socket.write(`HTTP/1.1 200 OK\n\n${body}`);
+            }
+              socket.end()
+          });
+            }else if(domain == "finset.io"){
+                socket.write('HTTP/1.1 301 Moved Permanently\nLocation: https://gitple.io/\n\n');
+                socket.end()
+            }else{
+                socket.write(`HTTP/1.1 200 OK\n\n<html><body><h1>you requested ${domain}</h1></body></html>`);
+                socket.end()
+            }
+	    console.log('Received: ' + data);
     });
 });
 
