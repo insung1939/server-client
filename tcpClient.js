21cd247d4b572fcea86273ed3f0b334e4d8c7d05
insungcho1939 <insungcho1939@gmail.com>
2021-09-05 12:41:13 +0900

    소스코드 리팩토링 및 수정

--- tcpClient.js
+++ tcpClient.js
@@ -1,22 +1,25 @@
-let net = require('net');
+let net = require("net");
 
-let options = { // 접속 정보 설정
+let options = {
+  // 접속 정보 설정
   port: 80,
-  host: "localhost"
-//     host: 'localhost'
+  host: "localhost",
 };
 
-let client = net.connect(options, () => { // 서버 접속
+let client = net.connect(options, () => {
+  // 서버 접속
   console.log("서버에 접속 성공했습니다.");
-  client.write('GET / HTTP/1.1\nHost: finset2.io\n\n')
+  client.write("GET / HTTP/1.1\nHost: finset2.io\n\n");
 });
 
 //client.send
 
-client.on('data', (data) => { // 데이터 수신 이벤트
+client.on("data", (data) => {
+  // 데이터 수신 이벤트
   console.log(data.toString());
 });
 
-client.on('end', () => { // 접속 종료
+client.on("end", () => {
+  // 접속 종료
   console.log("서버 접속이 종료되었습니다.");
 });
